fa847e2b46d3e7ab1d479a83c13c299b98be9009
Kalksomat <59163655+Kalksomat@users.noreply.github.com>
2025-01-16 16:02:07 +0100

    BLE Update 5

--- Arduino/BLE_Central_Device_3/BLE_Central_Device_3/BLE_Central_Device_3.ino
+++ Arduino/BLE_Central_Device_3/BLE_Central_Device_3/BLE_Central_Device_3.ino
@@ -1,5 +1,9 @@
 #include <ArduinoBLE.h>
 
+// Function prototypes
+void printPeripheralData(BLECharacteristic characteristic);
+void printData(const unsigned char data[], int length);
+
 void setup() {
   Serial.begin(9600);
   //while (!Serial);
@@ -37,8 +41,6 @@ void loop() {
       // stop scanning
       BLE.stopScan();
 
-      //explorerPeripheral(peripheral);
-
       Serial.println("Connecting ...");
 
       if (peripheral.connect()) {
@@ -65,41 +67,31 @@ void loop() {
       Serial.println(peripheral.appearance(), HEX);
       Serial.println();
 
-      /*BLEService service = peripheral.service(0);
-
-      Serial.print("Service ");
-      Serial.println(service.uuid());
-
-      Serial.print("\tCharacteristic ");
-      Serial.print(characteristic.uuid());
-      Serial.print(", properties 0x");
-      Serial.print(characteristic.properties(), HEX);
-      Serial.println();*/
-
       while (peripheral.localName() == "Peripheral Arduino") {
-  
-      bool serviceFound = false;
+        bool serviceFound = false;
 
-      // Iterate through all services
-      for (int i = 0; i < peripheral.serviceCount(); i++) {
-        BLEService service = peripheral.service(i);
+        // Iterate through all services
+        for (int i = 0; i < peripheral.serviceCount(); i++) {
+          BLEService service = peripheral.service(i);
 
-        // Iterate through all characteristics in this service
-        for (int j = 0; j < service.characteristicCount(); j++) {
-          BLECharacteristic characteristic = service.characteristic(j);
+          // Iterate through all characteristics in this service
+          for (int j = 0; j < service.characteristicCount(); j++) {
+            BLECharacteristic characteristic = service.characteristic(j);
 
-          // Check if the characteristic matches the target UUID
-          if (characteristic.uuid() == "e93df100-b754-4fda-adf3-5ca2ea89bde3") {
-            serviceFound = true;
+            // Check if the characteristic matches the target UUID
+            if (characteristic.uuid() == "e93df100-b754-4fda-adf3-5ca2ea89bde3") {
+              serviceFound = true;
 
-            // Print the characteristic data
-            printPeripheralData(characteristic);
+              // Print the characteristic data
+              printPeripheralData(characteristic);
+            }
           }
         }
-      }
-      // If no service or characteristic matches, print a message
-      if (!serviceFound) {
-        Serial.println("No service with the target UUID found");
+        // If no service or characteristic matches, print a message
+        if (!serviceFound) {
+          Serial.println("No service with the target UUID found");
+        }
+        Serial.println(peripheral.rssi());
       }
     }
   }
@@ -107,7 +99,6 @@ void loop() {
 }
 
 void printPeripheralData(BLECharacteristic characteristic) {
-
   if (characteristic.canRead()) {
     // read the characteristic value
     characteristic.read();
@@ -115,111 +106,10 @@ void printPeripheralData(BLECharacteristic characteristic) {
     if (characteristic.valueLength() > 0) {
       // print out the value of the characteristic
       printData(characteristic.value(), characteristic.valueLength());
-      //Serial.println(String(*characteristic.value()));
     }
   }
   Serial.println();
 }
-
-/*void explorerPeripheral(BLEDevice peripheral) {
-  // connect to the peripheral
-  Serial.println("Connecting ...");
-
-  if (peripheral.connect()) {
-    Serial.println("Connected");
-  } else {
-    Serial.println("Failed to connect!");
-    return;
-  }
-
-  // discover peripheral attributes
-  Serial.println("Discovering attributes ...");
-  if (peripheral.discoverAttributes()) {
-    Serial.println("Attributes discovered");
-  } else {
-    Serial.println("Attribute discovery failed!");
-    peripheral.disconnect();
-    return;
-  }
-
-  // read and print device name of peripheral
-  Serial.println();
-  Serial.print("Device name: ");
-  Serial.println(peripheral.deviceName());
-  Serial.print("Appearance: 0x");
-  Serial.println(peripheral.appearance(), HEX);
-  Serial.println();
-
-  // loop the services of the peripheral and explore each
-  for (int i = 0; i < peripheral.serviceCount(); i++) {
-    BLEService service = peripheral.service(i);
-
-    exploreService(service);
-  }
-
-  Serial.println();
-
-  // we are done exploring, disconnect
-  Serial.println("Disconnecting ...");
-  peripheral.disconnect();
-  Serial.println("Disconnected");
-}
-
-void exploreService(BLEService service) {
-  // print the UUID of the service
-  Serial.print("Service ");
-  Serial.println(service.uuid());
-
-  // loop the characteristics of the service and explore each
-  for (int i = 0; i < service.characteristicCount(); i++) {
-    BLECharacteristic characteristic = service.characteristic(i);
-
-    exploreCharacteristic(characteristic);
-  }
-}
-
-void exploreCharacteristic(BLECharacteristic characteristic) {
-  // print the UUID and properties of the characteristic
-  Serial.print("\tCharacteristic ");
-  Serial.print(characteristic.uuid());
-  Serial.print(", properties 0x");
-  Serial.print(characteristic.properties(), HEX);
-
-  // check if the characteristic is readable
-  if (characteristic.canRead()) {
-    // read the characteristic value
-    characteristic.read();
-
-    if (characteristic.valueLength() > 0) {
-      // print out the value of the characteristic
-      Serial.print(", value 0x");
-      printData(characteristic.value(), characteristic.valueLength());
-    }
-  }
-  Serial.println();
-
-  // loop the descriptors of the characteristic and explore each
-  for (int i = 0; i < characteristic.descriptorCount(); i++) {
-    BLEDescriptor descriptor = characteristic.descriptor(i);
-
-    exploreDescriptor(descriptor);
-  }
-}
-
-void exploreDescriptor(BLEDescriptor descriptor) {
-  // print the UUID of the descriptor
-  Serial.print("\t\tDescriptor ");
-  Serial.print(descriptor.uuid());
-
-  // read the descriptor value
-  descriptor.read();
-
-  // print out the value of the descriptor
-  Serial.print(", value 0x");
-  printData(descriptor.value(), descriptor.valueLength());
-
-  Serial.println();
-}*/
 
 void printData(const unsigned char data[], int length) {
   for (int i = 0; i < length; i++) {
@@ -229,7 +119,6 @@ void printData(const unsigned char data[], int length) {
       Serial.print("0");
     }
 
-    //Serial.print(b, HEX);
     Serial.print(b, HEX);
   }
 }
